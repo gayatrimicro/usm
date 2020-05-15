4caec0187030178a65ef46aed2c7ccad8fc33717
Rits-theDeveloper <ritesh.mestry.naresh@gmail.com>
2020-05-15 15:44:20 +0530

    Multiple language

--- header-backup.php
+++ header-backup.php
@@ -7,11 +7,14 @@
                 </a>
             </div>
             <div id="nav-wrapper">
-            	<ul id="nav">
+                
+
+                <ul id="nav">
                    <li class="list_li">
             <a href="#">Products</a> <i class="fa fa-angle-down" aria-hidden="true"></i>
             <div class="list_cat">
                 <ul>
+                <div class="half-column">
                     <li class="lis_li_li"><a href="#">Bagging Systems</a> <i class="fa fa-angle-right" aria-hidden="true"></i>
                         <div class="list_subcat rig_sid">
                             <ul>
@@ -26,7 +29,7 @@
                     </li>
                     <li><a href="http://spacegm.com/usm/products/transportable-bagging-system.php">TRANSPORATBLE BAGGING SYSTEMS</a></li>
                     <li class="lis_li_li"><a href="#">FFS (POUCH PACKING MACHINE)</a> <i class="fa fa-angle-right" aria-hidden="true"></i>
-                        <div class="list_subcat rig_sid3">
+                        <div class="list_subcat rig_sid3" >
                             <ul>
                                 <li><a href="http://spacegm.com/usm/products/ffs.php">WEIGHERS</a></li>
                                 <li><a href="http://spacegm.com/usm/products/ffs.php">FFS</a></li>
@@ -55,8 +58,11 @@
                     <li><a href="http://spacegm.com/usm/products/stretch-wrapping.php">STRETCH WRAPPERS</a></li>
                     <li><a href="http://spacegm.com/usm/products/bag-making.php">BAG MAKING MACHINES</a></li>
                     <li><a href="http://spacegm.com/usm/products/geo-text.php">GEO TEXT SEWING</a></li>
-                    <li><a href="http://spacegm.com/usm/products/carpet-sewing.php">CARPET SEWING</a></li>                               
-                    <li class="lis_li_li"><a href="#">CONVEYOR & ACCESSORIES</a> <i class="fa fa-angle-right" aria-hidden="true"></i>
+                </div>
+                <div class="half-column">
+
+                    <li><a href="http://spacegm.com/usm/products/carpet-sewing.php">CARPET SEWING</a></li>
+                   <li class="lis_li_li"><a href="#">CONVEYOR & ACCESSORIES</a> <i class="fa fa-angle-right" aria-hidden="true"></i>
                         <div class="list_subcat rig_sid4">
                             <ul>
                                 <li><a href="http://spacegm.com/usm/products/conveyor.php">DRAG CHAIN CONVEYOR</a></li>
@@ -85,13 +91,192 @@
                     <li><a href="http://spacegm.com/usm/products/dust-collector.php">DUST COLLECTORS</a></li>
                     <li><a href="http://spacegm.com/usm/products/thread.php">THREAD</a></li>
                     <!-- <li><a href="#">FUMIGATION SHEET</a></li> -->
-                    <li><a href="http://spacegm.com/usm/products/manganese-rail.php">MANGANESE RAIL</a></li>
+                    <li><a href="http://spacegm.com/usm/products/manganese-rail.php">MANGANESE RAIL</a></li> 
+                </div>
+                
+                                                   
+                    
                 </ul>
             </div>
         </li>
                     <li><a href="http://spacegm.com/usm/system_integration.php">System Integration</a></li>
                     <li><a href="http://spacegm.com/usm/spares-services.php">Spares & Service</a></li>
                     <li><a href="http://spacegm.com/usm/contact">Contact</a></li>
+                    <li>
+
+
+                        <style type="text/css">
+                            * {
+    box-sizing: border-box;
+}
+html,
+body {
+    height: 100%;
+}
+
+// .goog-te-banner-frame {display:none;} // Don't show Google Topbar
+// #goog-gt-tt {display:none!important;visibility:hidden!important;}
+
+#google_translate_element {
+
+
+    z-index: 99999;
+}
+.goog-te-gadget {
+    font-family: Roboto, "Open Sans", sans-serif !important;
+    text-transform: uppercase;
+}
+.goog-te-gadget-simple {
+    background-color: #fff0 !important;
+    border: 0px solid #fff0 !important;
+    color: #777 !important;
+    display: inline-block;
+    cursor: pointer;
+    zoom: 1;
+}
+.goog-te-menu-value{
+    color: #777 !important;
+}
+.goog-te-menu2 {
+    max-width: 100%;
+}
+
+.goog-te-menu-value span:nth-child(5) {
+    display: none;
+}
+.goog-te-menu-value span:nth-child(3) {
+    border: none !important;
+    font-family: "Material Icons";
+    &:after {
+        font-family: "Material Icons";
+        content: "\E5C5";
+        font-size: 1.5rem;
+        vertical-align: -6px;
+    }
+}
+
+.goog-te-gadget-icon {
+    background-image: url(https://placehold.it/32) !important;
+    background-position: 0px 0px;
+    
+    display: none;
+}
+
+.goog-te-gadget-simple{
+    padding: 0px !important;
+}
+
+// ============ HIDE TOP BAR ============
+.goog-te-banner-frame.skiptranslate {
+    display: none !important;
+}
+.goog-te-menu2-item div, .goog-te-menu2-item div span, .goog-te-menu2 {
+    background-color: #1b1b1b !important;
+    color: #b5b5b5 !important;
+    font-family: "brandon-grotesque",sans-serif !important;
+}
+body {
+    top: 0px !important;
+}
+
+/* ================================== *\
+    Mediaqueries
+\* ================================== */
+@media (max-width: 667px) {
+    #google_translate_element {
+        bottom: calc(100% - 50% - 53px);
+        left: 16px !important;
+        width: 100% !important;
+        goog-te-gadget {
+            width: 100% !important;
+        }
+        .skiptranslate {
+            width: 100% !important;
+        }
+        .goog-te-gadget-simple {
+            width: calc(100% - 32px) !important;
+            text-align: center;
+        }
+    }
+}
+
+                        </style>
+
+
+
+                        <div id="google_translate_element"></div>
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'ar,en,es,jv,ko,pt,ru,zh-CN', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false}, 'google_translate_element');
+}
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+
+<script type="text/javascript">
+    // WORK IN PROGRESS BELOW
+        
+$('document').ready(function () {
+
+
+        // RESTYLE THE DROPDOWN MENU
+    $('#google_translate_element').on("click", function () {
+
+        // Change font family and color
+        $("iframe").contents().find(".goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2 *")
+            .css({
+                'color': '#544F4B',
+                'font-family': 'Roboto',
+                                'width':'100%'
+            });
+        // Change menu's padding
+        $("iframe").contents().find('.goog-te-menu2-item-selected').css ('display', 'none');
+            
+                // Change menu's padding
+        $("iframe").contents().find('.goog-te-menu2').css ('padding', '0px');
+      
+        // Change the padding of the languages
+        $("iframe").contents().find('.goog-te-menu2-item div').css('padding', '20px');
+      
+        // Change the width of the languages
+        $("iframe").contents().find('.goog-te-menu2-item').css('width', '100%');
+        $("iframe").contents().find('td').css('width', '100%');
+      
+        // Change hover effects
+        $("iframe").contents().find(".goog-te-menu2-item div").hover(function () {
+            $(this).css('background-color', '#4385F5').find('span.text').css('color', 'white');
+        }, function () {
+            $(this).css('background-color', 'white').find('span.text').css('color', '#544F4B');
+        });
+
+        // Change Google's default blue border
+        $("iframe").contents().find('.goog-te-menu2').css('border', 'none');
+
+        // Change the iframe's box shadow
+        $(".goog-te-menu-frame").css('box-shadow', '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.3)');
+        
+      
+      
+        // Change the iframe's size and position?
+        $(".goog-te-menu-frame").css({
+            'height': '100%',
+            'width': '100%',
+            'top': '0px'
+        });
+        // Change iframes's size
+        $("iframe").contents().find('.goog-te-menu2').css({
+            'height': '100%',
+            'width': '100%'
+        });
+    });
+});
+
+</script>
+
+
+
+
+</li>
                 </ul>
             </div>
         </header>
