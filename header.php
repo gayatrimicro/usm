2051d8729338f5a8773696d1adaec2b208c24d36
Rits-theDeveloper <ritesh.mestry.naresh@gmail.com>
2020-05-15 16:21:19 +0530

    Multiple language

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
     
         <header>
             <div id="logo-holder">
-                <a id="logo" href="http://spacegm.com/usm/">
-                     <img src="http://spacegm.com/usm/img/USM.jpg"  >
+                <a id="logo" href="/">
+                     <img src="/img/USM.jpg"  >
                 </a>
             </div>
             <div id="nav-wrapper">
@@ -18,80 +18,80 @@
                     <li class="lis_li_li"><a href="#">Bagging Systems</a> <i class="fa fa-angle-right" aria-hidden="true"></i>
                         <div class="list_subcat rig_sid">
                             <ul>
-                                <li><a href="http://spacegm.com/usm/products/bagging.php#a">OPEN MOUTH</a></li>
-                                <li><a href="http://spacegm.com/usm/products/bagging.php#b">AUTOMATIC BAGGING LINES</a></li>
-                                <li><a href="http://spacegm.com/usm/products/bagging.php#c">IN PROCESS WEIGHING SYSTEM</a></li>
-                                <li><a href="http://spacegm.com/usm/products/bagging.php#d">VALVE (TYPE) BAGGING SYSTEMS</a></li>
-                                <li><a href="http://spacegm.com/usm/products/bagging.php#e">FIBC (BULK BAG FILLING SYSTEM)</a></li>
-                                <!-- <li><a href="http://spacegm.com/usm/products/bagging.php">JUMBO BIG BAG FILLING</a></li> -->
+                                <li><a href="/products/bagging.php#a">OPEN MOUTH</a></li>
+                                <li><a href="/products/bagging.php#b">AUTOMATIC BAGGING LINES</a></li>
+                                <li><a href="/products/bagging.php#c">IN PROCESS WEIGHING SYSTEM</a></li>
+                                <li><a href="/products/bagging.php#d">VALVE (TYPE) BAGGING SYSTEMS</a></li>
+                                <li><a href="/products/bagging.php#e">FIBC (BULK BAG FILLING SYSTEM)</a></li>
+                                <!-- <li><a href="/products/bagging.php">JUMBO BIG BAG FILLING</a></li> -->
                             </ul>
                         </div>
                     </li>
-                    <li><a href="http://spacegm.com/usm/products/transportable-bagging-system.php">TRANSPORATBLE BAGGING SYSTEMS</a></li>
+                    <li><a href="/products/transportable-bagging-system.php">TRANSPORATBLE BAGGING SYSTEMS</a></li>
                     <li class="lis_li_li"><a href="#">FFS (POUCH PACKING MACHINE)</a> <i class="fa fa-angle-right" aria-hidden="true"></i>
                         <div class="list_subcat rig_sid3" >
                             <ul>
-                                <li><a href="http://spacegm.com/usm/products/ffs.php">WEIGHERS</a></li>
-                                <li><a href="http://spacegm.com/usm/products/ffs.php">FFS</a></li>
+                                <li><a href="/products/ffs.php">WEIGHERS</a></li>
+                                <li><a href="/products/ffs.php">FFS</a></li>
                             </ul>
                         </div>
                     </li>  
-                    <li><a href="http://spacegm.com/usm/products/secondary-bagging.php">SECONDARY BAGGING (BAG INBAG)</a></li>
+                    <li><a href="/products/secondary-bagging.php">SECONDARY BAGGING (BAG INBAG)</a></li>
                                                                               
                     <li class="lis_li_li"><a href="#">BAG CLOSING SYSTEM</a> <i class="fa fa-angle-right" aria-hidden="true"></i>
                         <div class="list_subcat rig_sid2">
                             <ul>
-                                <li><a href="http://spacegm.com/usm/products/bag-closing.php">Bag Closing Machines</a></li>
-                                <li><a href="http://spacegm.com/usm/products/portable-bag-closing.php">Portable Bag Closing Machines</a></li>
+                                <li><a href="/products/bag-closing.php">Bag Closing Machines</a></li>
+                                <li><a href="/products/portable-bag-closing.php">Portable Bag Closing Machines</a></li>
                             </ul>
                         </div>
                     </li>
                     <li class="lis_li_li"><a href="#">Sealers</a> <i class="fa fa-angle-right" aria-hidden="true"></i>
                         <div class="list_subcat rig_sid6">
                             <ul>
-                                <li><a href="http://spacegm.com/usm/products/hot-air-sealer.php">Hot Air Sealers</a></li>
-                                <li><a href="http://spacegm.com/usm/products/band_sealer.php">Band Sealers</a></li>
+                                <li><a href="/products/hot-air-sealer.php">Hot Air Sealers</a></li>
+                                <li><a href="/products/band_sealer.php">Band Sealers</a></li>
                             </ul>
                         </div>
                     </li> 
-                    <li><a href="http://spacegm.com/usm/products/paletizers.php">PALLETIZERS</a></li>
-                    <li><a href="http://spacegm.com/usm/products/stretch-wrapping.php">STRETCH WRAPPERS</a></li>
-                    <li><a href="http://spacegm.com/usm/products/bag-making.php">BAG MAKING MACHINES</a></li>
-                    <li><a href="http://spacegm.com/usm/products/geo-text.php">GEO TEXT SEWING</a></li>
+                    <li><a href="/products/paletizers.php">PALLETIZERS</a></li>
+                    <li><a href="/products/stretch-wrapping.php">STRETCH WRAPPERS</a></li>
+                    <li><a href="/products/bag-making.php">BAG MAKING MACHINES</a></li>
+                    <li><a href="/products/geo-text.php">GEO TEXT SEWING</a></li>
                 </div>
                 <div class="half-column">
 
-                    <li><a href="http://spacegm.com/usm/products/carpet-sewing.php">CARPET SEWING</a></li>
+                    <li><a href="/products/carpet-sewing.php">CARPET SEWING</a></li>
                    <li class="lis_li_li"><a href="#">CONVEYOR & ACCESSORIES</a> <i class="fa fa-angle-right" aria-hidden="true"></i>
                         <div class="list_subcat rig_sid4">
                             <ul>
-                                <li><a href="http://spacegm.com/usm/products/conveyor.php">DRAG CHAIN CONVEYOR</a></li>
-                                <li><a href="http://spacegm.com/usm/products/conveyor.php">FLEXIBLE SCREW CONVEYOR</a></li>
-                                <li><a href="http://spacegm.com/usm/products/conveyor.php">STANDARD CONVEYOR</a></li>
-                                <li><a href="http://spacegm.com/usm/products/conveyor.php">LORRY LOADERS</a></li>
-                                <li><a href="http://spacegm.com/usm/products/conveyor.php#a">INCLINED BELT CONVEYOR</a></li>
-                                <li><a href="http://spacegm.com/usm/products/conveyor.php#a">TELESCOPIC CONVEYOR</a></li>
-                                <li><a href="http://spacegm.com/usm/products/conveyor.php#a">SCREW CONVEYOR</a></li>
+                                <li><a href="/products/conveyor.php">DRAG CHAIN CONVEYOR</a></li>
+                                <li><a href="/products/conveyor.php">FLEXIBLE SCREW CONVEYOR</a></li>
+                                <li><a href="/products/conveyor.php">STANDARD CONVEYOR</a></li>
+                                <li><a href="/products/conveyor.php">LORRY LOADERS</a></li>
+                                <li><a href="/products/conveyor.php#a">INCLINED BELT CONVEYOR</a></li>
+                                <li><a href="/products/conveyor.php#a">TELESCOPIC CONVEYOR</a></li>
+                                <li><a href="/products/conveyor.php#a">SCREW CONVEYOR</a></li>
                             </ul>
                         </div>
                     </li>
-                    <li><a href="http://spacegm.com/usm/products/bucket-elevator.php">BUCKET ELEVATOR</a></li>
-                    <li><a href="http://spacegm.com/usm/products/online-check-weigher.php">ONLINE CHECK WEIGHER</a></li>
+                    <li><a href="/products/bucket-elevator.php">BUCKET ELEVATOR</a></li>
+                    <li><a href="/products/online-check-weigher.php">ONLINE CHECK WEIGHER</a></li>
                     <li class="lis_li_li"><a href="#">METAL DETECTORS</a> <i class="fa fa-angle-right" aria-hidden="true"></i>
                         <div class="list_subcat rig_sid5">
                             <ul>
-                                <li><a href="http://spacegm.com/usm/products/metal-detector.php#a">ONLINE METAL DETECTORS</a></li>
-                                <li><a href="http://spacegm.com/usm/products/metal-detector.php#b">GRAVITY METAL DETECTORS</a></li>
+                                <li><a href="/products/metal-detector.php#a">ONLINE METAL DETECTORS</a></li>
+                                <li><a href="/products/metal-detector.php#b">GRAVITY METAL DETECTORS</a></li>
                             </ul>
                         </div>
                     </li>
-                    <li><a href="http://spacegm.com/usm/products/vaccum-packing-system.php">VACCUM PACKING SYSTEMS</a></li> 
-                    <li><a href="http://spacegm.com/usm/products/big-bag-dispenser.php">BIG BAG DISPENSER</a></li>
+                    <li><a href="/products/vaccum-packing-system.php">VACCUM PACKING SYSTEMS</a></li> 
+                    <li><a href="/products/big-bag-dispenser.php">BIG BAG DISPENSER</a></li>
                     
-                    <li><a href="http://spacegm.com/usm/products/dust-collector.php">DUST COLLECTORS</a></li>
-                    <li><a href="http://spacegm.com/usm/products/thread.php">THREAD</a></li>
+                    <li><a href="/products/dust-collector.php">DUST COLLECTORS</a></li>
+                    <li><a href="/products/thread.php">THREAD</a></li>
                     <!-- <li><a href="#">FUMIGATION SHEET</a></li> -->
-                    <li><a href="http://spacegm.com/usm/products/manganese-rail.php">MANGANESE RAIL</a></li> 
+                    <li><a href="/products/manganese-rail.php">MANGANESE RAIL</a></li> 
                 </div>
                 
                                                    
@@ -99,9 +99,9 @@
                 </ul>
             </div>
         </li>
-                    <li><a href="http://spacegm.com/usm/system_integration.php">System Integration</a></li>
-                    <li><a href="http://spacegm.com/usm/spares-services.php">Spares & Service</a></li>
-                    <li><a href="http://spacegm.com/usm/contact">Contact</a></li>
+                    <li><a href="/system_integration.php">System Integration</a></li>
+                    <li><a href="/spares-services.php">Spares & Service</a></li>
+                    <li><a href="/contact">Contact</a></li>
                     
                 </ul>
             </div>
